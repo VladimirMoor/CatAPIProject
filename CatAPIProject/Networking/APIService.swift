e00767a6eb7b74310a68857ae672d0eb7a59fbed
Vladimir Muravev <vladimircode@icloud.com>
2021-09-22 18:59:48 +0300

    Separate networking layer and error handling

---
+++ CatAPIProject/Networking/APIService.swift
@@ -0,0 +1,48 @@
+//
+//  APIService.swift
+//  CatAPIProject
+//
+//  Created by Владимир Муравьев on 22.09.2021.
+//
+
+import Foundation
+
+
+struct APIService {
+    func fetchBreeds(url: URL?, completion: @escaping(Result<[Breed], APIError>) -> Void ) {
+        
+        guard let url = url else {
+            let error = APIError.badURL
+            completion(Result.failure(error))
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if let error = error as? URLError {
+                completion(Result.failure(APIError.url(error)))
+                
+            } else if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
+                completion(Result.failure(APIError.badResponse(statusCode: response.statusCode)))
+                
+            } else if let data = data {
+                let decoder = JSONDecoder()
+                
+                 do {
+                     let breeds = try decoder.decode([Breed].self, from: data)
+                     
+                     completion(Result.success(breeds))
+                     
+                 } catch {
+                     
+                     completion(Result.failure(APIError.parsing(error as? DecodingError)))
+                     
+                 }
+                    
+            }
+        }
+        task.resume()
+        
+    }
+}
+
--- CatAPIProject/Networking/BreedFetcher.swift
+++ CatAPIProject/Networking/BreedFetcher.swift
@@ -21,34 +21,7 @@ class BreedFetcher: ObservableObject {
         
         isLoading = true
         
-        let url = URL(string: "https://api.thecatapi.com/v1/breeds")!
-
-        let task = URLSession.shared.dataTask(with: url) { [unowned self] (data, response, error) in
-            
-            if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
-                
-            }
-            
-            DispatchQueue.main.async {
-            self.isLoading = false
-            }
-            
-            if let data = data {
-                
-               let decoder = JSONDecoder()
-                do {
-                    let breeds = try decoder.decode([Breed].self, from: data)
-                    DispatchQueue.main.async {
-                        self.breeds = breeds
-                    }
-                } catch {
-                    print(error.localizedDescription)
-                }
-              }
-            
-        }
-
-        task.resume()
+   
         
     }
     
