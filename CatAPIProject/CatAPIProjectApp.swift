5c0a91d6524eba86f1dd4a8abe345d29fafcbf50
Vladimir Muravev <vladimircode@icloud.com>
2021-09-22 15:00:07 +0300

    Initial Commit

---
+++ CatAPIProject/CatAPIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAPIProjectApp.swift
+//  CatAPIProject
+//
+//  Created by Владимир Муравьев on 22.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CatAPIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
